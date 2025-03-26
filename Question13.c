9f303691803b5db1f222da7396c82799be1d724a
NELLURI_KOUSHIK <160224734+Nkoushik30@users.noreply.github.com>
2025-03-26 21:19:07 +0530

    Update Question13.c

--- Question13.c
+++ Question13.c
@@ -1,55 +1,49 @@
 #include <stdio.h>
-#include <string.h>  // Include this for strcmp()
+#include <string.h>
 
-struct fileTable 
-{
+struct fileTable {
     char name[20];
     int nob, blocks[30];
 } ft[30];
 
-int main() 
-{
+int main() {
     int i, j, n;
     char s[20];
-    
-    printf("Enter no of files :");
+
+    printf("\nEnter the number of files:\n");
     scanf("%d", &n);
 
-    for(i = 0; i < n; i++) 
-    {
-        printf("\nEnter file name %d: ", i+1);
+    for (i = 0; i < n; i++) {
+        printf("\nEnter file name:\n");
         scanf("%s", ft[i].name);
-        
-        printf("Enter no of blocks in file %d: ", i+1);
+
+        printf("\nEnter the number of blocks of the file:\n");
         scanf("%d", &ft[i].nob);
-        
-        printf("Enter the blocks of the file: ");
-        for(j = 0; j < ft[i].nob; j++)
+
+        printf("\nEnter the blocks of files:\n");
+        for (j = 0; j < ft[i].nob; j++) {
             scanf("%d", &ft[i].blocks[j]);
+        }
     }
 
-    printf("\nEnter the file name to be searched: ");
+    printf("\nEnter the file name to search: \n");
     scanf("%s", s);
 
-    for(i = 0; i < n; i++) 
-    {
-        if(strcmp(s, ft[i].name) == 0)
+    for (i = 0; i < n; i++) {
+        if (strcmp(s, ft[i].name) == 0) {
             break;
+        }
     }
 
-    if(i == n) //-
-        printf("\nFile Not Found\n");
-    else 
-    {
-        printf("\nFILE NAME \n");
-        printf("%s", ft[i].name);
-        printf("\nNumber of blocks\n");
-        printf("%d", ft[i].nob);
-        printf("\nallocated blocks\n");
-        for(j = 0; j < ft[i].nob; j++) 
-            printf("%d, ", ft[i].blocks[j]);
-
-        printf("\n");
+    if (i == n) {
+        printf("\nFile not found\n");
+    } else {
+        printf("\nFile Name: %s", ft[i].name);
+        printf("\nNumber of blocks: %d", ft[i].nob);
+        printf("\nAllocated blocks:\n");
+        for (j = 0; j < ft[i].nob; j++) {
+            printf("%d\n", ft[i].blocks[j]);
+        }
     }
 
     return 0;
